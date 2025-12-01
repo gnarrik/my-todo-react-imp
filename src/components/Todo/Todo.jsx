b6f4a5ac2e35cf0ae6f5b79c2d879d0e5c30c64d
Glygalo Nikita <gnarrik@gmail.com>
2025-12-01 13:44:03 +0700

    initial commit #2

---
+++ src/components/Todo/Todo.jsx
@@ -0,0 +1,19 @@
+import './Todo.scss'
+import AddTaskForm from '@/components/AddTaskForm';
+import SearchTaskForm from '@/components/SearchTaskForm';
+import TodoInfo from '@/components/TodoInfo';
+import TodoList from '@/components/TodoList';
+
+function Todo() {
+  return (
+      <div className='todo'>
+        <h1 className='todo__title'>To Do List</h1>
+        <AddTaskForm/>
+        <SearchTaskForm/>
+        <TodoInfo/>
+        <TodoList/>
+      </div>
+  )
+}
+
+export default Todo
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './styles/index.css'
+import Todo from '@/components/Todo';
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Todo/>
+  </StrictMode>,
+)
